1d5d8793f10a3493699e2bc351f1a46de8c05254
linatoro156 <linatoro156@gmail.com>
2018-12-10 09:22:23 -0300

    Add files via upload

--- sql/vw_dace_smartlist1.sql
+++ sql/vw_dace_smartlist1.sql
@@ -1,4 +1,15 @@
-/****** Object:  View [dbo].[vw_dace_smartlist1]    Script Date: 10/24/2018 16:38:07 ******/
+USE [MEX10]
+GO
+
+/****** Object:  View [dbo].[vw_dace_smartlist1]    Script Date: 12/06/2018 18:01:37 ******/
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[vw_dace_smartlist1]'))
+DROP VIEW [dbo].[vw_dace_smartlist1]
+GO
+
+USE [MEX10]
+GO
+
+/****** Object:  View [dbo].[vw_dace_smartlist1]    Script Date: 12/06/2018 18:01:37 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -7,40 +18,40 @@ GO
 
 CREATE VIEW [dbo].[vw_dace_smartlist1]
 AS
-SELECT     a.DCSTATUS, e.ESTATUS, CASE a.doctype WHEN 1 THEN 'Factura' WHEN 6 THEN 'Pago' END AS tipo, a.DOCTYPE, a.CNTRLNUM, a.DOCNUMBR, a.DOCDATE, 
-                      a.VENDORID, e.DOCAMNT, e.CURNCYID, CFDI.FORMAPAGO, B.MexFolioFiscal AS CFDI_ASOCIADO, B1.MexFolioFiscal AS CFDI_APLICADO, 
-                      CASE A.DOCTYPE WHEN 1 THEN OK.CFDI_UUID WHEN 6 THEN OK.UUIDRELACIONADO END AS CFDI_APLICAR, 
-                      CASE WHEN CASE A.DOCTYPE WHEN 1 THEN OK.CFDI_UUID WHEN 6 THEN OK.UUIDRELACIONADO END = B1.MexFolioFiscal THEN 'APLICACION CORRECTA' ELSE 'APLICACION INCORRECTA'
-                       END AS OBSERVACIONES
-FROM         dbo.PM00400 AS a LEFT OUTER JOIN
-                          (SELECT     VCHRNMBR, VENDORID, DOCTYPE, DOCDATE, DOCNUMBR, DOCAMNT, CURTRXAM, DISTKNAM, DISCAMNT, DSCDLRAM, BACHNUMB, TRXSORCE, 
-                                                   BCHSOURC, DISCDATE, DUEDATE, PORDNMBR, TEN99AMNT, WROFAMNT, DISAMTAV, TRXDSCRN, UN1099AM, BKTPURAM, BKTFRTAM, BKTMSCAM, 
-                                                   VOIDED, HOLD, CHEKBKID, DINVPDOF, PPSAMDED, PPSTAXRT, PGRAMSBJ, GSTDSAMT, POSTEDDT, PTDUSRID, MODIFDT, MDFUSRID, PYENTTYP, 
-                                                   CARDNAME, PRCHAMNT, TRDISAMT, MSCCHAMT, FRTAMNT, TAXAMNT, TTLPYMTS, CURNCYID, PYMTRMID, SHIPMTHD, TAXSCHID, PCHSCHID, 
-                                                   FRTSCHID, MSCSCHID, PSTGDATE, DISAVTKN, CNTRLTYP, NOTEINDX, PRCTDISC, RETNAGAM, ICTRX, Tax_Date, PRCHDATE, CORRCTN, SIMPLIFD, 
-                                                   BNKRCAMT, APLYWITH, Electronic, ECTRX, DocPrinted, TaxInvReqd, VNDCHKNM, BackoutTradeDisc, CBVAT, VADCDTRO, TEN99TYPE, 
-                                                   TEN99BOXNUMBER, PONUMBER, DEX_ROW_TS, DEX_ROW_ID, 'OPEN' AS ESTATUS, '' AS CFDI_APLICADO, 0 AS CFDI_D
-                            FROM          dbo.PM20000 AS o
+SELECT     m.CNTRLTYP, m.DOCTYPE, m.CNTRLNUM, m.DOCNUMBR, m.DOCDATE, m.VENDORID, a.DOCAMNT, a.CURNCYID, a.TRXDSCRN, 
+                      CASE m.DOCTYPE WHEN 6 THEN CASE WHEN MAX(CFDIA.METODOPAGO) = 'PUE' AND MAX(isnull(asoc.MexFolioFiscal, '')) = '' THEN MAX(APLICADO.MexFolioFiscal) 
+                      ELSE MAX(asoc.MexFolioFiscal) END ELSE MAX(asoc.MexFolioFiscal) END AS CFDI_ASOC, CASE m.DOCTYPE WHEN 1 THEN CASE WHEN (CFDI.METODOPAGO) 
+                      = 'PUE' THEN MAX(asoc.MexFolioFiscal) ELSE MAX(aplicado.MexFolioFiscal) END WHEN 6 THEN CASE WHEN (CFDIa.METODOPAGO) 
+                      = 'PUE' THEN MAX(aplicado.MexFolioFiscal) ELSE MAX(aplicado.MexFolioFiscal) END END AS CFDI_Apli, ISNULL(CFDI.METODOPAGO, '') AS MetodoPago, 
+                      CFDIA.METODOPAGO AS cfdia_metodopago, CASE WHEN CFDI.METODOPAGO = 'PUE' OR
+                      CFDIA.METODOPAGO = 'PUE' THEN CASE WHEN m.DOCTYPE = 1 AND ISNULL(MAX(aplicado.MexFolioFiscal), '') = '' AND MAX(a.DocAplic) IS NOT NULL 
+                      THEN 'CFDI SE APLICO OK' ELSE CASE WHEN m.DOCTYPE = 6 AND ISNULL(MAX(asoc.MexFolioFiscal), '') 
+                      = '' THEN 'CFDI SE APLICO OK' ELSE 'CFDI SE APLICO ERRADO' END END ELSE CASE WHEN MIN(dbo.fun_DACE_ValidarAplica(CASE m.DOCTYPE WHEN 1 THEN aplicado.MexFolioFiscal
+                       WHEN 6 THEN asoc.MexFolioFiscal END, CASE m.DOCTYPE WHEN 1 THEN asoc.MexFolioFiscal WHEN 6 THEN aplicado.MexFolioFiscal END)) 
+                      > 0 THEN 'CFDI SE APLICO OK' ELSE 'CFDI SE APLICO ERRADO' END END AS OBSERVACIONES, m.DCSTATUS
+FROM         dbo.PM00400 AS m LEFT OUTER JOIN
+                          (SELECT     o.VCHRNMBR, o.VENDORID, o.DOCTYPE, o.DOCDATE, o.DOCNUMBR, o.DOCAMNT, o.CURTRXAM, o.TRXDSCRN, o.VOIDED, o.CURNCYID, 
+                                                   'OPEN' AS ESTATUS, CASE o.DOCTYPE WHEN 6 THEN B.APTVCHNM WHEN 1 THEN B.VCHRNMBR END AS DocAplic, 
+                                                   CASE o.DOCTYPE WHEN 6 THEN B.APTODCTY WHEN 1 THEN B.DOCTYPE END AS DocTipoAplic
+                            FROM          dbo.PM20000 AS o LEFT OUTER JOIN
+                                                   dbo.PM20100 AS B ON CASE o.DOCTYPE WHEN 6 THEN B.DOCTYPE WHEN 1 THEN B.APTODCTY END = o.DOCTYPE AND 
+                                                   CASE o.DOCTYPE WHEN 6 THEN B.VCHRNMBR WHEN 1 THEN B.APTVCHNM END = o.VCHRNMBR
                             UNION
-                            SELECT     A1.VCHRNMBR, A1.VENDORID, A1.DOCTYPE, A1.DOCDATE, A1.DOCNUMBR, A1.DOCAMNT, A1.CURTRXAM, A1.DISTKNAM, A1.DISCAMNT, A1.DSCDLRAM, 
-                                                  A1.BACHNUMB, A1.TRXSORCE, A1.BCHSOURC, A1.DISCDATE, A1.DUEDATE, A1.PORDNMBR, A1.TEN99AMNT, A1.WROFAMNT, A1.DISAMTAV, 
-                                                  A1.TRXDSCRN, A1.UN1099AM, A1.BKTPURAM, A1.BKTFRTAM, A1.BKTMSCAM, A1.VOIDED, A1.HOLD, A1.CHEKBKID, A1.DINVPDOF, A1.PPSAMDED, 
-                                                  A1.PPSTAXRT, A1.PGRAMSBJ, A1.GSTDSAMT, A1.POSTEDDT, A1.PTDUSRID, A1.MODIFDT, A1.MDFUSRID, A1.PYENTTYP, A1.CARDNAME, 
-                                                  A1.PRCHAMNT, A1.TRDISAMT, A1.MSCCHAMT, A1.FRTAMNT, A1.TAXAMNT, A1.TTLPYMTS, A1.CURNCYID, A1.PYMTRMID, A1.SHIPMTHD, A1.TAXSCHID, 
-                                                  A1.PCHSCHID, A1.FRTSCHID, A1.MSCSCHID, A1.PSTGDATE, A1.DISAVTKN, A1.CNTRLTYP, A1.NOTEINDX, A1.PRCTDISC, A1.RETNAGAM, A1.VOIDPDATE, 
-                                                  A1.ICTRX, A1.Tax_Date, A1.PRCHDATE, A1.CORRCTN, A1.SIMPLIFD, A1.APLYWITH, A1.Electronic, A1.ECTRX, A1.DocPrinted, A1.TaxInvReqd, 
-                                                  A1.VNDCHKNM, A1.BackoutTradeDisc, A1.CBVAT, A1.VADCDTRO, A1.TEN99TYPE, A1.TEN99BOXNUMBER, A1.PONUMBER, A1.DEX_ROW_TS, 
-                                                  A1.DEX_ROW_ID, 'HIST' AS Expr1, ISNULL(B.APTVCHNM, B1.VCHRNMBR) AS Expr2, ISNULL(B.APTODCTY, B1.DOCTYPE) AS Expr3
-                            FROM         dbo.PM30200 AS A1 LEFT OUTER JOIN
-                                                  dbo.PM30300 AS B ON B.DOCTYPE = A1.DOCTYPE AND B.VCHRNMBR = A1.VCHRNMBR LEFT OUTER JOIN
-                                                  dbo.PM30300 AS B1 ON B1.APTODCTY = A1.DOCTYPE AND B1.APTVCHNM = A1.VCHRNMBR) AS e ON e.DOCTYPE = a.DOCTYPE AND 
-                      e.VCHRNMBR = a.CNTRLNUM LEFT OUTER JOIN
-                      dbo.ACA_IETU00400 AS B ON B.DOCTYPE = a.DOCTYPE AND B.VCHRNMBR = a.CNTRLNUM LEFT OUTER JOIN
-                      dace.ComprobanteCFDI AS CFDI ON CFDI.TIPOCOMPROBANTE = CASE B.DOCTYPE WHEN 1 THEN '1' WHEN 6 THEN 'P' END AND 
-                      CFDI.UUID = B.MexFolioFiscal LEFT OUTER JOIN
-                      dbo.ACA_IETU00400 AS B1 ON B1.DOCTYPE = e.CFDI_D AND B1.VCHRNMBR = e.CFDI_APLICADO LEFT OUTER JOIN
-                      dace.ComprobanteCFDIRelacionado AS OK ON CASE A.DOCTYPE WHEN 1 THEN OK.UUIDRELACIONADO WHEN 6 THEN OK.CFDI_UUID END = B.MexFolioFiscal
-WHERE     (e.VOIDED = 0)
+                            SELECT     o.VCHRNMBR, o.VENDORID, o.DOCTYPE, o.DOCDATE, o.DOCNUMBR, o.DOCAMNT, o.CURTRXAM, o.TRXDSCRN, o.VOIDED, o.CURNCYID, 
+                                                  'HIST' AS ESTATUS, CASE o.DOCTYPE WHEN 6 THEN B.APTVCHNM WHEN 1 THEN B.VCHRNMBR END AS Expr1, 
+                                                  CASE o.DOCTYPE WHEN 6 THEN B.APTODCTY WHEN 1 THEN B.DOCTYPE END AS Expr2
+                            FROM         dbo.PM30200 AS o LEFT OUTER JOIN
+                                                  dbo.PM30300 AS B ON CASE o.DOCTYPE WHEN 6 THEN B.DOCTYPE WHEN 1 THEN B.APTODCTY END = o.DOCTYPE AND 
+                                                  CASE o.DOCTYPE WHEN 6 THEN B.VCHRNMBR WHEN 1 THEN B.APTVCHNM END = o.VCHRNMBR) AS a ON m.DOCTYPE = a.DOCTYPE AND 
+                      m.CNTRLNUM = a.VCHRNMBR LEFT OUTER JOIN
+                      dbo.ACA_IETU00400 AS asoc ON asoc.DOCTYPE = a.DOCTYPE AND asoc.VCHRNMBR = a.VCHRNMBR LEFT OUTER JOIN
+                      dbo.ACA_IETU00400 AS aplicado ON aplicado.DOCTYPE = a.DocTipoAplic AND aplicado.VCHRNMBR = a.DocAplic LEFT OUTER JOIN
+                      dace.ComprobanteCFDI AS CFDI ON CFDI.TIPOCOMPROBANTE = CASE M.DOCTYPE WHEN 6 THEN 'P' WHEN 1 THEN 'I' END AND 
+                      CFDI.UUID = asoc.MexFolioFiscal LEFT OUTER JOIN
+                      dace.ComprobanteCFDI AS CFDIA ON CFDIA.TIPOCOMPROBANTE = CASE M.DOCTYPE WHEN 6 THEN 'I' END AND CFDIA.UUID = aplicado.MexFolioFiscal
+WHERE     (a.VOIDED = 0)
+GROUP BY m.DOCTYPE, m.CNTRLNUM, m.DOCNUMBR, m.DOCDATE, m.VENDORID, a.DOCAMNT, a.CURNCYID, a.TRXDSCRN, asoc.MexFolioFiscal, CFDI.METODOPAGO, 
+                      CFDIA.METODOPAGO, m.DCSTATUS, m.CNTRLTYP
 
 GO
 
@@ -69,7 +80,7 @@ Begin DesignProperties =
       End
       Begin PaneConfiguration = 5
          NumPanes = 2
-         Configuration = "(H (2[33] 3) )"
+         Configuration = "(H (2[65] 3) )"
       End
       Begin PaneConfiguration = 6
          NumPanes = 2
@@ -116,7 +127,7 @@ Begin DesignProperties =
          Left = 0
       End
       Begin Tables = 
-         Begin Table = "a"
+         Begin Table = "m"
             Begin Extent = 
                Top = 6
                Left = 38
@@ -126,22 +137,32 @@ Begin DesignProperties =
             DisplayFlags = 280
             TopColumn = 0
          End
-         Begin Table = "e"
+         Begin Table = "a"
             Begin Extent = 
                Top = 6
                Left = 248
                Bottom = 114
-               Right = 420
+               Right = 399
             End
             DisplayFlags = 280
             TopColumn = 0
          End
-         Begin Table = "B"
+         Begin Table = "asoc"
             Begin Extent = 
                Top = 6
                Left = 458
                Bottom = 114
                Right = 609
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "aplicado"
+            Begin Extent = 
+               Top = 6
+               Left = 865
+               Bottom = 114
+               Right = 1016
             End
             DisplayFlags = 280
             TopColumn = 0
@@ -156,22 +177,12 @@ Begin DesignProperties =
             DisplayFlags = 280
             TopColumn = 0
          End
-         Begin Table = "B1"
-            Begin Extent = 
-               Top = 6
-               Left = 865
-               Bottom = 114
-               Right = 1016
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-         Begin Table = "OK"
+         Begin Table = "CFDIA"
             Begin Extent = 
                Top = 114
                Left = 38
-               Bottom = 207
-               Right = 248
+               Bottom = 222
+               Right = 218
             End
             DisplayFlags = 280
             TopColumn = 0
@@ -187,10 +198,10 @@ Begin DesignProperties =
          Width = 284
          Width = 1500
          Width = 1500
-         Width = 150' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vw_dace_smartlist1'
+        ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vw_dace_smartlist1'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'0
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N' Width = 1575
          Width = 1500
          Width = 1500
          Width = 1500
@@ -201,7 +212,7 @@ EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'0
          Width = 1500
          Width = 1500
          Width = 1500
-         Width = 1500
+         Width = 1980
          Width = 1500
          Width = 1500
          Width = 1500
@@ -270,7 +281,7 @@ EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'0
    End
    Begin CriteriaPane = 
       PaneHidden = 
-      Begin ColumnWidths = 11
+      Begin ColumnWidths = 12
          Column = 1440
          Alias = 900
          Table = 1170
